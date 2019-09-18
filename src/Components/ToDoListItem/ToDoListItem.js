25aab5c5aa5be3e129ea56d20f3109a05cc696be
darkraven987 <darkraven987@gmail.com>
2019-09-18 16:09:19 +0300

    Item number output fix

--- src/Components/ToDoListItem/ToDoListItem.js
+++ src/Components/ToDoListItem/ToDoListItem.js
@@ -3,10 +3,10 @@ import editIcon from '../../img/edit_icon.svg';
 import deleteIcon from '../../img/delete_icon.png';
 
 export const ToDoListItem = (props) => {
-  const { item } = props;
+  const { item, number } = props;
   return(
       <div className="itemContainer">
-        <div className="itemNumber">{item.id}.</div>
+        <div className="itemNumber">{number+1}.</div>
         <div className="itemTitle">{item.title}</div>
         <div className="itemControlPanel">
           <div className="iconContainer edit"><img src={ editIcon } alt="###"/></div>
