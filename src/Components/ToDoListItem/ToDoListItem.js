77506719567b87840ab4bdff04860ef089cb46c6
darkraven987 <darkraven987@gmail.com>
2019-09-18 13:31:19 +0300

    Got basic item output

--- src/Components/ToDoListItem/ToDoListItem.js
+++ src/Components/ToDoListItem/ToDoListItem.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import editIcon from '../../img/edit_icon.svg';
+import deleteIcon from '../../img/delete_icon.png';
 
 export const ToDoListItem = (props) => {
   const { item } = props;
   return(
+      <div className="itemContainer">
+        <div className="itemNumber">{item.id}.</div>
+        <div className="itemTitle">{item.title}</div>
+        <div className="itemControlPanel">
+          <div className="iconContainer edit"><img src={ editIcon } alt="###"/></div>
+          <div className="iconContainer delete"><img src={ deleteIcon } alt="###"/></div>
+        </div>
+      </div>
   )
 }
