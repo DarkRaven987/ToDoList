4208b28832c82dbb972036a370dd6fde688df574
darkraven987 <darkraven987@gmail.com>
2019-09-19 15:05:31 +0300

    Buttons basic interaction added

--- src/Components/ToDoListItem/ToDoListItem.js
+++ src/Components/ToDoListItem/ToDoListItem.js
@@ -1,23 +1,56 @@
 import React from 'react';
 import MaterialIcon from 'material-icons-react'
 
-export const ToDoListItem = (props) => {
-  const { item, number } = props;
-  return(
-      <div className="itemContainer">
-        <div className="itemNumber">{number+1}.</div>
-        <div className="itemTitle">{item.title}</div>
-        <div className="itemControlPanel">
-            <div className="iconContainer done">
-                <MaterialIcon icon="done" invert/>
-            </div>
-          <div className="iconContainer edit">
-              <MaterialIcon icon="edit" invert/>
+class ToDoListItem extends React.Component {
+  constructor(props){
+      super(props);
+
+      this.state =  {
+          status: 'neutral',
+      }
+  }
+
+    setItemDone = () => {
+        this.setState({ status: 'done'});
+    };
+
+    setItemUndone = () => {
+        this.setState({ status: 'undone'});
+    };
+
+  render(){
+      const { number, item} = this.props;
+      const { status } = this.state,
+          isNeutral = status === 'neutral',
+          isDone = status === 'done',
+          isUndone = status === 'undone';
+
+      return(
+          <div className={ isDone ? "itemContainer doneItem" : isUndone ? "itemContainer undoneItem" : 'itemContainer' }>
+              <div className="itemNumber">{number+1}.</div>
+              <div className="itemTitle">{item.title}</div>
+              <div className="itemControlPanel">
+
+                  <div className="iconContainer edit">
+                      <MaterialIcon icon="edit" invert/>
+                  </div>
+
+                  { !isDone && (
+                      <div className="iconContainer done" onClick={() => this.setItemDone() }>
+                          <MaterialIcon icon="done" invert/>
+                      </div>
+                  )}
+
+                  { !isUndone && (
+                      <div className="iconContainer delete undone">
+                          <MaterialIcon  icon= "close" invert onClick={ () => this.setItemUndone() }/>
+                      </div>
+                  )}
+              </div>
           </div>
-          <div className="iconContainer delete">
-              <MaterialIcon icon="delete_forever" invert/>
-          </div>
-        </div>
-      </div>
-  )
+      )
+  }
 }
+
+
+export default ToDoListItem;
