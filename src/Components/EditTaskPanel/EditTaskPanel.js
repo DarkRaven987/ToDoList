f7a90e281c206b1a251436d949dd972ca777671f
darkraven987 <darkraven987@gmail.com>
2019-09-19 16:00:31 +0300

    Added component to edit task

---
+++ src/Components/EditTaskPanel/EditTaskPanel.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+class EditTaskPanel extends React.Component {
+    constructor(props){
+        super(props);
+        const { todos } = this.props;
+        this.state = {
+            newTask: {
+                id: todos[todos.length-1].id+1,
+                title: '',
+                isDone: false
+            }
+        }
+    }
+
+    changeTaskTitle = (e) => {
+        this.setState({newTask: {
+                ...this.state.newTask,
+                title: e.target.value
+            }});
+    };
+
+    buttonClickHandler = ( isAdd = false ) => {
+        let titleCondition = this.state.newTask.title.length > 4;
+
+        if ( isAdd ) {
+            titleCondition ?
+                this.props.addTask(this.state.newTask)
+                :
+                alert('Task title is too short. Should be more than 4 symbols.');
+        }
+
+        this.setState({newTask: {
+                ...this.state.newTask,
+                title: ''
+            }});
+    };
+
+    render(){
+        return(
+            <div className="addTaskContainer">
+                <h3>Task #:</h3>
+                <input id="editTask" type="text" placeholder="Enter task title" value={ this.state.newTask.title } onChange={ (e) => this.changeTaskTitle(e) }/>
+                <div className="newTaskControlPanel">
+                    <div className="newTaskButton Add" onClick={ () => this.buttonClickHandler(true) }>Add</div>
+                    <div className="newTaskButton Clear" onClick={ () => this.buttonClickHandler() }>Clear</div>
+                </div>
+            </div>
+
+        )
+    }
+};
+
+export default EditTaskPanel;
