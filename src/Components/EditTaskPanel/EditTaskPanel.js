5c46c75afd11c50443863a74b1441d74706b52ce
darkraven987 <darkraven987@gmail.com>
2019-09-20 14:12:26 +0300

    Component simplyfication and text edit bug fix

--- src/Components/EditTaskPanel/EditTaskPanel.js
+++ src/Components/EditTaskPanel/EditTaskPanel.js
@@ -1,56 +1,37 @@
 import React from 'react';
 
-class EditTaskPanel extends React.Component {
-    constructor(props){
-        super(props);
+export const EditTaskPanel = (props) => {
 
-        this.state = {
-            editingTask: {}
-        }
-    }
+    const { editTaskNumber, setEditingTaskTitle, getEditingTaskTitle, getEditingTask } = props;
 
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        return nextProps.editingTask.id !== this.state.editingTask.id
-    }
 
-    changeTaskTitle = (e) => {
-        this.setState({editingTask: {
-                ...this.state.editingTask,
-                title: e.target.value
-            }});
-    };
-
-    buttonClickHandler = ( isEdit = false ) => {
-        let titleCondition = this.state.editingTask.title.length > 4;
+    const buttonClickHandler = ( isEdit = false ) => {
+        let titleCondition = getEditingTaskTitle().length > 4;
 
         if ( isEdit ) {
-            titleCondition ?
-                this.props.editTask(this.state.editingTask)
-                :
-                alert('Task title is too short. Should be more than 4 symbols.');
+            if ( titleCondition ) {
+                props.editTask( getEditingTask() );
+                props.setEditingTask({});
+                props.setPanelMode(true);
+            } else alert('Task title is too short. Should be more than 4 symbols.');
+        } else {
+            props.setEditingTask({});
+            props.setPanelMode(true);
         }
 
-        this.props.setEditingTask({});
-        this.props.setPanelMode(true);
     };
 
-    render(){
-        const { id, title } = this.state.editingTask;
-
-        this.setState({editingTask: this.props.editingTask});
-
-        return(
-            <div className="addTaskContainer">
-                <h3>Task #{ id }:</h3>
-                <input id="editTask" type="text" placeholder="Enter task title" value={ title } onChange={ (e) => this.changeTaskTitle(e) }/>
-                <div className="newTaskControlPanel">
-                    <div className="newTaskButton Add" onClick={ () => this.buttonClickHandler(true) }>Edit</div>
-                    <div className="newTaskButton Clear" onClick={ () => this.buttonClickHandler() }>Cancel</div>
-                </div>
+    return(
+        <div className="addTaskContainer">
+            <h3>Task #{ editTaskNumber() + 1 }:</h3>
+            <input id="editTask" type="text" placeholder="Enter task title" value={ getEditingTaskTitle() } onChange={ (e) => setEditingTaskTitle(e) }/>
+            <div className="newTaskControlPanel">
+                <div className="newTaskButton Add" onClick={ () => buttonClickHandler(true) }>Edit</div>
+                <div className="newTaskButton Clear" onClick={ () => buttonClickHandler() }>Cancel</div>
             </div>
+        </div>
 
-        )
-    }
+    )
 };
 
 export default EditTaskPanel;
