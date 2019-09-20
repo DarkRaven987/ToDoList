a017ceb882ad9f49086ac8ee17c0787a499baa3c
darkraven987 <darkraven987@gmail.com>
2019-09-20 11:35:33 +0300

    Component simplyfied

--- src/Components/ToDoListItem/ToDoListItem.js
+++ src/Components/ToDoListItem/ToDoListItem.js
@@ -1,40 +1,38 @@
 import React from 'react';
 import MaterialIcon from 'material-icons-react'
 
-class ToDoListItem extends React.Component {
+const ToDoListItem = ( props ) => {
 
-  render(){
-      const { number, item, setEditingTask, setTaskDone, setTaskUndone, } = this.props;
-      let isNeutral = item.isDone === 'neutral',
-          isDone = item.isDone === 'done',
-          isUndone = item.isDone === 'undone';
+  const { number, item, setEditingTask, setTaskDone, setTaskUndone, } = props;
+  let isNeutral = item.isDone === 'neutral',
+      isDone = item.isDone === 'done',
+      isUndone = item.isDone === 'undone';
 
 
-      return(
-          <div className={ isDone ? "itemContainer doneItem" : isUndone ? "itemContainer undoneItem" : 'itemContainer' }>
-              <div className="itemNumber">{number+1}.</div>
-              <div className="itemTitle">{item.title}</div>
-              <div className="itemControlPanel">
+  return(
+      <div className={ isDone ? "itemContainer doneItem" : isUndone ? "itemContainer undoneItem" : 'itemContainer' }>
+          <div className="itemNumber">{number+1}.</div>
+          <div className="itemTitle">{item.title}</div>
+          <div className="itemControlPanel">
 
-                  <div className="iconContainer edit" onClick={() => setEditingTask(item)}>
-                      <MaterialIcon icon="edit" invert/>
-                  </div>
-
-                  { !isDone && (
-                      <div className="iconContainer done" onClick={() => setTaskDone(item.id) }>
-                          <MaterialIcon icon="done" invert/>
-                      </div>
-                  )}
-
-                  { !isUndone && (
-                      <div className="iconContainer delete undone">
-                          <MaterialIcon  icon= "close" invert onClick={ () => setTaskUndone(item.id) }/>
-                      </div>
-                  )}
+              <div className="iconContainer edit" onClick={() => setEditingTask(item)}>
+                  <MaterialIcon icon="edit" invert/>
               </div>
+
+              { !isDone && (
+                  <div className="iconContainer done" onClick={() => setTaskDone(item.id) }>
+                      <MaterialIcon icon="done" invert/>
+                  </div>
+              )}
+
+              { !isUndone && (
+                  <div className="iconContainer delete undone">
+                      <MaterialIcon  icon= "close" invert onClick={ () => setTaskUndone(item.id) }/>
+                  </div>
+              )}
           </div>
-      )
-  }
+      </div>
+  )
 }
 
 
