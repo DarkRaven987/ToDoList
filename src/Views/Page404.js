ac59b78563ea16f5efe5627183a7dd78814d73f1
darkraven987 <darkraven987@gmail.com>
2019-10-02 16:47:44 +0300

    Done with 404 page

--- src/Views/Page404.js
+++ src/Views/Page404.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import MaterialIcon from 'material-icons-react'
+import {Link} from "react-router-dom";
 
 const Page404 = () => {
   return(
     <div className="container">
       <div className="content_container error_page">
-
+        <div className="greatSizeTitle">
+          ERROR 404: Page not found
+        </div>
+        <Link className="ErrorPageLink" to="/">
+            <MaterialIcon icon="keyboard_backspace"/>
+            <span>Go back to tasks list </span>
+        </Link>
       </div>
     </div>
   )
