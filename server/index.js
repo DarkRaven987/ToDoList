b8e87c23ac96f508fcea2458466ff76b5ce9533e
darkraven987 <darkraven987@gmail.com>
2019-10-02 12:55:22 +0300

    Base db connection installed

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
+const mysql          = require("mysql2");
 const bodyParser     = require('body-parser');
+const db             = require('./config');
 const app            = express();
 const port           = 3001;
-app.use(bodyParser.urlencoded({ extended: true }));
-require('./routes')(app, {});
 
-app.listen(port, () => {
-    console.log('We are live on ' + port);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const connection = mysql.createConnection({
+    host: db.host,
+    user: db.user,
+    database: db.database,
+    password: db.password
+});
+
+connection.connect(function(err){
+    if (err) {
+        return console.error("Ошибка: " + err.message);
+    }
+
+    require('./routes');
+
+    app.listen(port, () => {
+        console.log('We are live on ' + port);
+    });
 });
