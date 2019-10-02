ca680d147df550c4e6005f1a4de38f76e4c3a33a
darkraven987 <darkraven987@gmail.com>
2019-10-02 15:38:56 +0300

    Completed router instalment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 import { Header } from './Components/Header/Header';
-import { Footer } from './Components/Footer/Footer';
-import ToDoList from './Components/ToDoList/ToDoList';
+import ToDoList from './Views/ToDoList';
+import LoginForm from './Views/LoginForm';
+import Page404 from './Views/Page404'
 import './App.css';
-
 
 const App = () => {
 
-    return (
-        <div className="App">
+  return (
+    <div className="App">
+      <Router>
+        <div>
           <Header/>
 
-          <ToDoList/>
-
-          <Footer/>
+          <Switch>
+            <Route path="/" exact>
+              <ToDoList/>
+            </Route>
+            <Route path="/login">
+              <LoginForm/>
+            </Route>
+            <Route>
+              <Page404/>
+            </Route>
+          </Switch>
         </div>
-    );
+      </Router>
+    </div>
+  );
 };
 
 export default App;
--- src/Components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Footer = () => {
-  return(
-      <div className="headerAndFooter todoFooter">
-        A damn lot of text in here
-      </div>
-  )
-};
