9ec3c79223e2a60cc3fde6318a97d23df03b6f27
darkraven987 <darkraven987@gmail.com>
2019-10-03 13:11:50 +0300

    Finished base for login page

--- src/Views/LoginForm.js
+++ src/Views/LoginForm.js
@@ -1,11 +1,42 @@
 import React from 'react';
 
-const LoginForm = () => {
-  return(
-    <div>
-      <h1>Login form</h1>
-    </div>
-  )
+class LoginForm extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <div className="content_container loginForm">
+          <div className="middleSizeTitle borderWithTitle">
+          <span className="middleSizeTitle">
+            Login
+          </span>
+            <form className="loginFormContent" action="">
+              <div className="formItemContainer">
+                <label htmlFor="username" style={{fontSize: 25}} >Username</label>
+                <input type="text" id="username" autoComplete="off"/>
+              </div>
+              <div className="formItemContainer">
+                <label htmlFor="password" style={{fontSize: 25}} >Password</label>
+                <input type="password" id="password"/>
+              </div>
+              <div className="controlPanel greatPadding">
+                <div className="Button Clear middlePadding" style={{margin: 0}}>Login</div>
+                <div className="Button Alert middlePadding" style={{margin: 0}}>Register</div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
 };
 
 export default LoginForm;
