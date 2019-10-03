d00c1c54297005cb1eb767319e4df2e62f216ec4
darkraven987 <darkraven987@gmail.com>
2019-10-03 11:27:26 +0300

    Style classes refactor

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Header = () => {
   return(
-      <div className="headerAndFooter header">
+      <div className="header">
         <h2>To Do List Website</h2>
       </div>
   )
