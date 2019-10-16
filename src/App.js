57cad97f303a182775c9bcf2bb5b652dced2a24f
darkraven987 <darkraven987@gmail.com>
2019-10-16 20:21:08 +0300

    Added redirect if not logged in

--- src/App.js
+++ src/App.js
@@ -6,8 +6,18 @@ import ToDoList from './Views/ToDoList';
 import LoginForm from './Views/LoginForm';
 import Page404 from './Views/Page404'
 import './App.css';
+import {getCookie} from "./utility";
+
+
+function redirectIfNotLogged() {
+  if(document.location.href!=='http://'+document.location.host+'/login' && !getCookie('user_id')) {
+    document.location.href = "/login"
+  }
+}
 
 const App = () => {
+
+  redirectIfNotLogged();
 
   return (
     <div className="App">
