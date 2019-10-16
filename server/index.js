10cf060e31c08a9c9f56bde61d008b398ff40360
darkraven987 <darkraven987@gmail.com>
2019-10-16 20:20:03 +0300

    Added headers for free comunications

--- server/index.js
+++ server/index.js
@@ -3,9 +3,15 @@ const mysql          = require("mysql2");
 const bodyParser     = require('body-parser');
 const db             = require('./config');
 const app            = express();
-const port           = 3001;
+const port           = 7777;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //DB CONNECTION INSTALL
 const connection = mysql.createConnection({
