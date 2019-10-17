759e4165e71c6fd992a63ba466198c47d4487fe8
darkraven987 <darkraven987@gmail.com>
2019-10-17 10:49:43 +0300

    Added route to register new user

--- server/routes/route_users.js
+++ server/routes/route_users.js
@@ -9,12 +9,32 @@ module.exports = function(app, db) {
 
     app.post('/users/auth', (req, res) => {
         let username = req.body.name,
-            hash = req.body.hash,
-            query = `SELECT user_id FROM users WHERE user_name = '${username}' AND user_hash = '${hash}'`;
+          hash = req.body.hash,
+          query = `SELECT user_id FROM users WHERE user_name = '${username}' AND user_hash = '${hash}'`;
 
         db.query(query, function (error, result, fields) {
             if (error) res.send(error);
             res.send(result);
+        });
+    });
+
+    app.post('/users/register', (req, res) => {
+        let username = req.body.name,
+          hash = req.body.hash,
+          checkQuery = `SELECT user_id FROM users WHERE user_name = '${username}'`,
+          query = `INSERT INTO users (user_name, user_hash, user_created, user_updated) VALUES ('${username}', '${hash}', NOW(), NOW())`;
+
+
+        db.query(checkQuery, function (error, result, fields) {
+            if (error) console.log(error);
+            if (result.length) {
+                res.send("0");
+                return;
+            }
+            db.query(query, function (error, result, fields) {
+                if (error) res.send(error);
+                res.send("1");
+            });
         });
     });
 };
