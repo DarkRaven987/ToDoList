44d044342fce4e4c02245958bf715c848c59d4c5
darkraven987 <darkraven987@gmail.com>
2019-10-17 10:48:58 +0300

    Can logout of logged in

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {getCookie} from "../../utility";
 
 export const Header = () => {
   return(
       <div className="header">
         <h2>To Do List Website</h2>
+        {
+          getCookie("user_id") && <h3 onClick={() => {
+            document.cookie='user_id=0; max-age=0';
+            document.location.href = '/login';
+          }}>Logout</h3>
+        }
       </div>
   )
 }
