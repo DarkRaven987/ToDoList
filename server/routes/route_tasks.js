673e8f2d428872bf773e63a14904d3a094360da0
darkraven987 <darkraven987@gmail.com>
2019-10-17 12:04:16 +0300

    Code refactor

--- server/routes/route_tasks.js
+++ server/routes/route_tasks.js
@@ -1,8 +1,8 @@
 module.exports = function(app, db) {
 
-    app.get('/tasks', (req, res) => {
-        let user_id = req.params.id,
-            query = `SELECT * FROM tasks WHERE user_id = '${user_id}'`;
+    app.post('/tasks', (req, res) => {
+        let user_id = req.body.user_id,
+            query = `SELECT task_title, task_description, task_status FROM tasks WHERE user_id = '${user_id}'`;
         db.query( query, function(error, result, fields){
             if (error) res.send(error);
             res.send(result);
