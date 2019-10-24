21e3999bad17795a7fa60dfce6562b6de4532eba
darkraven987 <darkraven987@gmail.com>
2019-10-24 16:06:15 +0300

    Changed task add process

--- server/routes/route_tasks.js
+++ server/routes/route_tasks.js
@@ -11,8 +11,8 @@ module.exports = function(app, db) {
 
     app.post('/tasks/add', (req, res) => {
         let {task_title, user_id,} = req.body,
-          query = `INSERT INTO tasks (task_title, task_description, task_status, user_id, task_created_date, task_updated_date) 
-          VALUES ('${task_title}', 'test', 'neutral', '${user_id}', NOW(), NOW())`,
+          query = `INSERT INTO tasks (task_title, task_description, task_status, category_id, user_id, task_created_date, task_updated_date) 
+          VALUES ('${task_title}', 'test', 'neutral', '5','${user_id}', NOW(), NOW())`,
           newTaskIndexQ = `SELECT task_id, task_title, task_description, task_status FROM tasks WHERE user_id = '${user_id}'`;
 
         db.query( query, function(error, result, fields){
